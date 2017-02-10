1e3f143384790cc7baec52c68882714fd7843755
Daniele Biggiogero <daniele@skebby.it>
2017-02-11 00:12:39 +0100

    fix moving cubes bug, edit gdd

--- super pallina/Assets/scripts/moving_cubes.cs
+++ super pallina/Assets/scripts/moving_cubes.cs
@@ -7,29 +7,36 @@ public class moving_cubes : MonoBehaviour {
 	private bool trigger;
 	public float maximum; 
 	static float t = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 		trigger = false;
-		maximum = transform.localPosition.z - 3f;
+		maximum = transform.position.z - 3f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (trigger == true){
-			transform.position = new Vector3(transform.localPosition.x, transform.localPosition.y, Mathf.Lerp(transform.localPosition.z, maximum, t));
+			
+			transform.position = new Vector3(
+				transform.position.x,
+				transform.position.y,
+				Mathf.Lerp(
+					transform.position.z,
+					maximum,
+					t
+				)
+			);
+
 			t += 0.3f * Time.deltaTime;
-
 		}
-
 	}
 
 	void OnTriggerEnter(Collider collision){
 
 		if (collision.gameObject.tag == "Player") {
-			Debug.Log ("player");
+			Debug.Log ("Cube entered");
 			trigger = true;
 		}
-	
-	
 	}
 }
